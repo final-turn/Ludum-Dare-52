b35bd2b28d433635b28773824da2bda75c628b28
David Carranza <dcarranza.business@gmail.com>
2023-01-06 21:20:24 -0800

    Added basic UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("My Stats")]
+    public int population;
+    public int satisfaction;
+
+    [Header("Resource")]
+    public int metalCount;
+    public int foodCount;
+    public int energyCount;
+
+    [Header("UI")]
+    public TMP_Text populationUI;
+    public TMP_Text satisfactionUI;
+    public TMP_Text metalCountUI;
+    public TMP_Text foodCountUI;
+    public TMP_Text energyCountUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        populationUI.text = "" + population + "k";
+        satisfactionUI.text = "" + satisfaction + "%";
+        metalCountUI.text = "" + metalCount;
+        foodCountUI.text = "" + foodCount;
+        energyCountUI.text = "" + energyCount;
+    }
+
+    public void GoToNextDay()
+    {
+
+    }
+}
